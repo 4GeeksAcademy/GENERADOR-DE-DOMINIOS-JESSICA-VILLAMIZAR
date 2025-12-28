33aa90d185bd8a14d5bd0efee0720d8f0a6084b8
1989212 <jessica_villamizar.c@hotmail.com>
2025-12-28 22:38:51 +0000

    proyecto

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+// console.log(ext.slice(1));
+
+
+let pronombres = ['the', 'our', 'my'];
+let adjetivos = ['great', 'big', 'best', 'smart'];
+let sustantivos = ['jogger', 'racoon', 'developer', 'coder', 'genius'];
+let extensiones = ['.com', '.net', '.us', '.io', '.org', '.dev']; 
+
+
+
+pronombres.forEach(pron => {
+ adjetivos.forEach(adj => {
+  sustantivos.forEach(sust => {
+      let nombreDeDominios = pron+ adj+ sust ; 
+         extensiones.forEach(ext => {
+            if(sust.endsWith(ext.slice(1))){
+               console.log(pron+ adj+sust.slice(0,-ext.slice(1).length)+ ext)
+               
+            } else{
+              let nombreDeDominiosConExt = nombreDeDominios + ext;
+              console.log(nombreDeDominiosConExt)
+            }
+             
+
+         })
+      })
+   })
+});
